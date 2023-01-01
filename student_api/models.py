cf3b03b05ecd6d51142cc9fac171ac0549792f7a
birkan <b.561birkan@gmail.com>
2023-01-01 19:52:34 +0300

    creating new table in db and binding one-to-many relation between tables

--- student_api/models.py
+++ student_api/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Path(models.Model):
+    path_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return f"{self.path_name}"
+
 class Student(models.Model):
     first_name = models.CharField(max_length = 25)
     last_name = models.CharField(max_length = 50)
     number = models.IntegerField()
     age = models.IntegerField(blank = True, null = True)
+    path = models.ForeignKey(Path, related_name = "students", on_delete = models.CASCADE)
 
     def __str__(self):
         return self.first_name
