22591baeec098d02972f3949baf7f9c603dff970
birkan <b.561birkan@gmail.com>
2023-01-07 22:35:51 +0300

    LimitOffsetPagination

--- student_api/pagination.py
+++ student_api/pagination.py
@@ -1,7 +1,12 @@
 from rest_framework.pagination import (
-    PageNumberPagination
+    PageNumberPagination,
+    LimitOffsetPagination
 )
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 5
     page_query_param = "sayfa"
+
+
+class CustomLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
--- student_api/views.py
+++ student_api/views.py
@@ -231,13 +231,14 @@ class StudentDetailCV(generics.RetrieveUpdateDestroyAPIView):
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 
-from .pagination import CustomPageNumberPagination
+from .pagination import CustomPageNumberPagination, CustomLimitOffsetPagination
 
 class StudentModelViewSet(ModelViewSet):
 
     serializer_class = StudentSerializer
     queryset = Student.objects.all()
-    pagination_class = CustomPageNumberPagination
+    # pagination_class = CustomPageNumberPagination
+    pagination_class = CustomLimitOffsetPagination
 
     @action(detail = False, methods = ["GET"])
     def student_count(self, request):
