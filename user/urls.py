2aabcd52154223e36b09231712144ba773d4869b
birkan <b.561birkan@gmail.com>
2023-01-09 21:30:35 +0300

    creating new token for a registered user by exposing an api endpoint

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .views import RegisterView
 
+from rest_framework.authtoken import views
+
 urlpatterns = [
     path("register/", RegisterView.as_view()),
+    path("login/", views.obtain_auth_token),  # creating new token for user by exposing an api endpoint
 ]
